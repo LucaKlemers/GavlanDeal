77127be0442f2a03fe1f3f9953c6de22e39de581
Luca <luca.klemmers@gmail.com>
2025-07-14 13:19:31 +0300

    Added proper table and form

--- GavlanDealApp/views.py
+++ GavlanDealApp/views.py
@@ -4,6 +4,7 @@ from integration_utils.bitrix24.functions.call_list_method import call_list_meth
 from integration_utils.bitrix24.functions.api_call import api_call
 from django.conf import settings
 from operator import itemgetter
+import requests
 
 @main_auth(on_start=True, set_cookie=True)
 def index(request):
@@ -17,18 +18,20 @@ def index(request):
 @main_auth(on_cookies=True)
 def last_deals(request):
     token = request.bitrix_user_token
-    full_deals = sorted(call_list_method(token, 'crm.deal.list', ['select => ["ID", "TITLE", "OPPORTUNITY", "TAX_VALUE", "BEGINDATE", "CLOSEDATE", "UF_CRM_1752242366887"]']), key=itemgetter('LAST_ACTIVITY_TIME'), reverse=True)[:10]
+    # full_deals = sorted(call_list_method(token, 'crm.deal.list', ['select => ["ID", "TITLE", "OPPORTUNITY", "TAX_VALUE", "BEGINDATE", "CLOSEDATE", "UF_CRM_1752242366887"]']), key=itemgetter('LAST_ACTIVITY_TIME'), reverse=True)[:10]
     fields = ["ID", "Наименование", "Валюта", "Сумма", "Налог",
              # "Клиент",
               "Начало", "Конец",  "Скидка(%)"]
     deal_types = ["ID", "TITLE", "CURRENCY_ID", "OPPORTUNITY", "TAX_VALUE", "BEGINDATE", "CLOSEDATE", "UF_CRM_1752242366887"]
+    full_deals = sorted(token.call_api_method("crm.deal.list")['result'], key=itemgetter('LAST_ACTIVITY_TIME'), reverse=True)[:10]
     deals = []
-    print(full_deals)
     for deal in full_deals:
+        id= {'ID': deal["ID"]}
+        real_deal = token.call_api_method('crm.deal.get', params=id)['result']
         values = []
-        for value in deal:
+        for value in real_deal:
             if value in deal_types:
-                values.append(deal[value])
+                values.append(real_deal[value])
             #elif value == "CONTACT_ID":
             #    values.append(call_list_method(token, "crm.contact.get", fields=[int(deal[value])])[1]['NAME'])
         deals.append(values)
@@ -38,6 +41,7 @@ def last_deals(request):
 @main_auth(on_cookies=True)
 def add_deal(request):
     token = request.bitrix_user_token
-    if request.POST:
-        api_call('b24-klxrii.bitrix24.ru', 'crm.deal.add', token, params=[request])
+    if request.method == "POST":
+        print(request.POST.dict())
+        token.call_api_method('crm.deal.add', {"fields": request.POST.dict()})
     return render(request, 'GavlanDealApp/add_deal.html')
