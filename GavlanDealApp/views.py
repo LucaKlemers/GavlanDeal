edd05bd295f2f357668a407a3103b448986d26ba
Luca <luca.klemmers@gmail.com>
2025-07-16 18:29:03 +0300

    Added an employee table

--- GavlanDealApp/views.py
+++ GavlanDealApp/views.py
@@ -12,7 +12,10 @@ import requests
 from PIL import Image
 import io
 import base64
-
+from functools import lru_cache
+from random import randint
+from datetime import datetime, timedelta, timezone
+from time import sleep
 @main_auth(on_start=True, set_cookie=True)
 def index(request):
     app_settings = settings.APP_SETTINGS
@@ -64,8 +67,8 @@ def generate_qr(request):
         image_url = "https://b24-klxrii.bitrix24.ru" + str(product['PROPERTY_44'][0]['value']['downloadUrl'])
         response = requests.get(image_url, stream=True)
         image = Image.open(io.BytesIO(response.content))
-        image_path = f"GavlanDealApp\product_images\{id}.webp"
-        image.save(f"GavlanDealApp\static\GavlanDealApp\product_images\{id}.webp")
+        image_path = rf"GavlanDealApp\product_images\{id}.webp"
+        image.save(rf"GavlanDealApp\static\GavlanDealApp\product_images\{id}.webp")
 
         product_db, _ = Product.objects.get_or_create(product_id=id, defaults = {'product_name': name, 'product_description': descritpion, 'product_image': image_path})
         product_db.save()
@@ -120,11 +123,63 @@ def product_list(request):
     context = {"fields": fields, "products": products,}
     return render(request, 'GavlanDealApp/product_list.html', context)
 
+# def add_calls(employees, token):
+#     for employee in employees:
+#         calls = randint(1, 10)
+#         call_time = (datetime.now(timezone.utc) - timedelta(hours=1)).isoformat()
+#         for i in range (calls):
+#             employee_id = employee['id']
+#             call_start = token.call_api_method("telephony.externalcall.register",
+#                                                params={"PHONE_NUMBER":"1", "USER_ID": employee_id, "CALL_START_DATE": call_time, "TYPE": 1})['result']
+#             call_end = token.call_api_method("telephony.externalcall.finish", params={"CALL_ID": call_start["CALL_ID"], "USER_ID": employee_id, "DURATION": randint(1, 200)})
+#     return call_end
+
 @main_auth(on_cookies=True)
 def employees(request):
     token = request.bitrix_user_token
-    users = token.call_api_method("im.department.colleagues.list")['result']
-    print(users)
+    departments = token.call_api_method("department.get")['result']
+
     employees = []
-    context = {"employees": employees}
+    #@lru_cache(maxsize=None)
+    def get_heads (department, heads):
+
+        department_id = department['ID']
+        department_heads = token.call_api_method("im.department.managers.get", params={'ID': [int(department_id)], "USER_DATA": "Y"})['result']
+        if department_heads:
+            heads += department_heads[department_id]
+        if 'PARENT' in department:
+            return get_heads([d for d in departments if d['ID'] == department['PARENT']][0], heads)
+        else:
+            return heads
+
+    for department in departments:
+        all_heads = get_heads(department, [])
+        employee_list = token.call_api_method("im.department.employees.get",  params={"ID": [int(department['ID'])], "USER_DATA": "Y"})['result']
+        if employee_list:
+            for employee in employee_list[department['ID']]:
+                employee_id = employee['id']
+                employee_name = employee['name']
+                employee_heads = []
+                for head in all_heads:
+                    if head['id'] != employee_id:
+                        employee_heads.append(head['name'])
+                duplicates = list(filter(lambda e: e['id'] == employee_id, employees))
+                if duplicates:
+                    duplicates[0]['heads'].append(employee_heads)
+                elif employee['active']:
+                    now = datetime.now(timezone.utc)
+                    earliest = (now - timedelta(hours=24)).isoformat()
+                    calls = token.call_api_method("voximplant.statistic.get", params={"FILTER": {">CALL_DURATION": 60, ">CALL_START_DATE": earliest}})['result']
+                    employees.append({"name": employee_name, "heads": employee_heads, "id": employee_id, "calls": len(calls),})
+
+    # add_calls(employees, token)
+
+    employee_list = []
+    for employee in employees:
+        employee_list.append([employee['name'], employee['heads'], employee['calls'], employee["id"]])
+
+    context = {"employees": employee_list, }
     return render(request, 'GavlanDealApp/employees.html', context)
+
+
+
